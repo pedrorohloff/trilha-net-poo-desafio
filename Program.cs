88bd1baec51e3eb545f982240ba341eca4374813
Pedro Rohloff <pedro.h.rohloff@gmail.com>
2023-12-18 13:34:08 -0300

    Desafio POO em DotNet Concluido!!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+//  Testes em ambos as classes realizado, agindo como esperado.
+
+Console.WriteLine("Smartphone Nokia");
+
+Nokia celularNokia = new Nokia("4758-1245", "X30", "449279095666730", 4096);
+
+celularNokia.Ligar();
+celularNokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+Console.WriteLine("Smartphone iPhone");
+
+Iphone celularIphone = new Iphone("4457-9543", "iPhone 11", "353995102427923", 4096);
+
+celularIphone.ReceberLigacao();
+celularIphone.InstalarAplicativo("Facebook");
